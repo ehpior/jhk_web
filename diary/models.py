feccd0989d2d8c88e9415447a61499014da1e6f0
ehpior <ehpior@kookmin.ac.kr>
2019-01-02 15:40:21 +0900

    diary

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Ddate(models.Model):
+    pub_date = models.DateTimeField('published')
+
+    def __str__(self):
+        return self.pub_date
+
+class Ddiary(models.Model):
+    ddate = models.ForeignKey(Ddate, on_delete = models.CASCADE)
+    dtitle = models.CharField(max_length = 50)
+    dcontent = models.TextField()
+
+    def __str__(self):
+        return self.dtitle
