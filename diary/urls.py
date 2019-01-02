2b3644ce0ee9beaa82b9204c08b06b998f039fab
ehpior <ehpior@kookmin.ac.kr>
2019-01-02 17:19:51 +0900

    diary

--- diary/urls.py
+++ diary/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'diary'
 
 urlpatterns = [
-    #path('', views.index, name='index'),
-    #path('<int:question_id>/', views.detail, name='detail'),
-    #path('<int:question_id>/results/', views.results, name='results'),
-    #path('<int:question_id>/make/', views.vote, name='vote'),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('create/', views.create, name='create'),
+    path('create/make/', views.make, name='make'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -10,23 +10,27 @@ def index(request):
     return render(request, 'diary/index.html', context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'diary/detail.html', {'question': question})
+    diary_content = get_object_or_404(Question, pk=question_id)
+    return render(request, 'diary/detail.html', {'diary_content': diary_content})
 
-#def vote(request, question_id):
-#    question = get_object_or_404(Question, pk=question_id)
-#    try:
-#        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-#    except (KeyError, Choice.DoesNotExist):
-#        return render(request, 'polls/detail.html', {
-#            'question': question,
-#            'error_message': "You didn't select a choice.",
-#        })
-#    else:
-#        selected_choice.votes += 1
-#        selected_choice.save()
-#        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-#
-#def results(request, question_id):
-#    question = get_object_or_404(Question, pk=question_id)
-#    return render(request, 'polls/results.html', {'question': question})
+
+def create(request):
+    #question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'diary/create.html')
+
+
+'''
+def make(request):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+'''
